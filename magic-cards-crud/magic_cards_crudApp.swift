02ed068e356609a9c9f3a0cf18554269253aa891
Alfredo Paes da Luz <alfredo.alfpaes@gmail.com>
2023-10-05 22:58:08 -0300

    Initial Commit

---
+++ magic-cards-crud/magic_cards_crudApp.swift
@@ -0,0 +1,20 @@
+//
+//  magic_cards_crudApp.swift
+//  magic-cards-crud
+//
+//  Created by Alfredo Paes da Luz on 05/10/23.
+//
+
+import SwiftUI
+
+@main
+struct magic_cards_crudApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
